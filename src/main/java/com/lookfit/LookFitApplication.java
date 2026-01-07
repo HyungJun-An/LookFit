bf5ca5b0ef97bc7468d126593120be6e1286d014
안형준 <anhyeongjun@anhyeongjun-ui-MacBookAir.local>
2026-01-07 15:35:48 +0900

    first commit

---
+++ src/main/java/com/lookfit/LookFitApplication.java
@@ -0,0 +1,13 @@
+package com.lookfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookFitApplication.class, args);
+    }
+
+}
